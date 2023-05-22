68516d28773fe9eb3a869a6bfb2fa3a85ab17e8c
Seo <seochoi56@gmail.com>
2023-05-22 18:02:41 -0400

    fixed blank page issue and added Navlink

--- src/App.js
+++ src/App.js
@@ -1,59 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
-import Home from './components/Home'
-import Contact from './components/Contact'
-import Demo from './components/Demo'
-import SkillsAndCert from './components/SkillAndCert'
+import Contact from './components/Contact';
+import Demo from './components/Demo';
+import SkillsAndCert from './components/SkillAndCert';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <BrowserRouter>
-        <div id="browserNavContainer">
-          <div className="NavBlocks">
-            <NavLink>
-              Home
-            </NavLink>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <div id="browserNavContainer">
+            <div className="NavBlocks">
+              <NavLink to="/" style={({ isActive }) => 
+                ({ color: isActive ? 'greenyellow' : 'white' })}>
+                  Home
+              </NavLink>
+            </div>
+            <div className="NavBlocks">
+              <NavLink to="/demo" style={({ isActive }) => 
+                ({ color: isActive ? 'greenyellow' : 'white' })}>
+                  Demo
+              </NavLink>
+            </div>
+            <div className="NavBlocks">
+              <NavLink to="/skills&cert" style={({ isActive }) => 
+                ({ color: isActive ? 'greenyellow' : 'white' })}>
+                  Skills and Certifications
+              </NavLink>
+            </div>
+            <div className="NavBlocks">
+              <NavLink to="/contact" style={({ isActive }) => 
+                ({ color: isActive ? 'greenyellow' : 'white' })}>
+                  Contact
+              </NavLink>
+            </div>
           </div>
-          <div className="NavBlocks">
-          <NavLink>
-              Demo
-            </NavLink>
-          </div>
-          <div className="NavBlocks">
-          <NavLink>
-              Skills and Certifications
-            </NavLink>
-          </div>
-          <div className="NavBlocks">
-          <NavLink>
-              Contact
-            </NavLink>
-          </div>
-        </div>
-        <Routes>
-          <Route exact path="/" element={<App />} />
-          <Route exact path="/demo" element={<Demo />} />
-          <Route exact path="/skills&cert" element={<SkillsAndCert />} />
-          <Route exact path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Edit my site 
-        </a>
-      </header>
-    </div>
+          <Routes>
+            <Route exact path="/demo" element={<Demo />} />
+            <Route exact path="/skills&cert" element={<SkillsAndCert />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Edit my site
+          </a>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
